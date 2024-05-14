319b471c0ea4ac9de2c8aea436a05bf5bfb61db6
carlos fernandez <43886531+cajava13@users.noreply.github.com>
2024-05-14 17:03:25 -0300

    Append new Terraform module for Security02

--- terraform/main.tf
+++ terraform/main.tf
@@ -50,3 +50,28 @@ module "aaaa" {
 
   account_customizations_name = "security-aaaa"
 }
+
+
+module "Security02" {
+  source = "./modules/aft-account-request"
+
+  control_tower_parameters = {
+    AccountEmail              = "escala.iact.us+security2@escala24x7.com"
+    AccountName               = "Security02"
+    ManagedOrganizationalUnit = "Sandbox"
+    SSOUserEmail              = "escala.iact.us+security2@escala24x7.com"
+    SSOUserFirstName          = "admin"
+    SSOUserLastName           = "account"
+  }
+
+  account_tags = {
+    "OU" = "Sandbox"
+  }
+
+  change_management_parameters = {
+    change_requested_by = "Security02"
+    change_reason       = "testing"
+  }
+
+  account_customizations_name = "sandbox"
+}
