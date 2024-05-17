8e0292ca1af5246ca0dab9cef92b9d2e09e0a129
carlos fernandez <43886531+cajava13@users.noreply.github.com>
2024-05-17 11:59:29 -0300

    Append new Terraform module for seguridadB

--- terraform/main.tf
+++ terraform/main.tf
@@ -25,3 +25,28 @@ module "sandbox" {
 
   account_customizations_name = "sandbox"
 }
+
+
+module "seguridadB" {
+  source = "./modules/aft-account-request"
+
+  control_tower_parameters = {
+    AccountEmail              = "escala.iact.us+secb@escala24x7.com"
+    AccountName               = "seguridadB"
+    ManagedOrganizationalUnit = "Sandbox"
+    SSOUserEmail              = "escala.iact.us+secb@escala24x7.com"
+    SSOUserFirstName          = "seguridad"
+    SSOUserLastName           = "seguridad"
+  }
+
+  account_tags = {
+    "OU" = "Sandbox"
+  }
+
+  change_management_parameters = {
+    change_requested_by = "seguridadB"
+    change_reason       = "newaccount"
+  }
+
+  account_customizations_name = "Security-seguridadB"
+}
